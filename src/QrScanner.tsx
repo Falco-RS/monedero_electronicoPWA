bbba3fb04db5b1682cd841fb09b7252bdb8b0e8e
MikelRojas <131219015+MikelRojas@users.noreply.github.com>
2024-10-22 21:19:07 -0600

    Update QrScanner.tsx

--- src/QrScanner.tsx
+++ src/QrScanner.tsx
@@ -1,6 +1,7 @@
 // src/QrScanner.tsx
 import React, { useRef, useEffect, useState } from 'react';
 import jsQR from 'jsqr';
+import { getIdClient } from './Store';
 
 const QrScanner: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -28,6 +29,16 @@ const QrScanner: React.FC = () => {
               const code = jsQR(imageData.data, canvas.width, canvas.height);
               if (code) {
                 setResult(code.data);
+                console.log(code.data)
+                if(code.data){
+                  const url = new URL(code.data);
+                  const idString: string | null = url.searchParams.get('id');
+                  const idNumber: number | null = idString ? Number(idString) : null;
+                  if(idNumber===getIdClient()){
+                    console.log("si es igual")
+                    verification(code.data)
+                  }
+                }
               }
             }
           }
@@ -41,6 +52,22 @@ const QrScanner: React.FC = () => {
     }
   }, []);
 
+  const verification = async (apiUrl: string) => {
+    try {
+      const response = await fetch(apiUrl);
+      
+      // Verificamos que la respuesta sea exitosa
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log(data);
+    } catch (error: any) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div>
       <video ref={videoRef} style={{ display: 'none' }} />
@@ -51,4 +78,3 @@ const QrScanner: React.FC = () => {
 };
 
 export default QrScanner;
-
